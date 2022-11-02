42900c9826ab58a543373fd25140a156b8e8a6d1
heeaepark <pha060611@gmail.com>
2022-11-02 20:34:41 +0900

    component Windowbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './styles/global.css'; //global style import
 import './styles/theme.css'; // theme style import
+import WindowBar from './components/WindowBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <WindowBar />
     </div>
   );
 }
--- src/components/WindowBar.js
+++ src/components/WindowBar.js
@@ -1,10 +1,29 @@
-import ReactIcon from './icons/ReactIcon';
+import VscodeIcon from './icons/VscodeIcon';
 import style from './../styles/WindowBar.module.css';
 
 const WindowBar = () => {
   return (
-    <div>
-
+    <div className={style.windowbar}>
+      <div className={style.window_btn_wrap}>
+        <VscodeIcon className={style.icon} fill={'#2196f3'} />
+        <div className={style.btn_items}>
+          <p className={style.btn_item}>File</p>
+          <p className={style.btn_item}>Edit</p>
+          <p className={style.btn_item}>View</p>
+          <p className={style.btn_item}>Go</p>
+          <p className={style.btn_item}>Run</p>
+          <p className={style.btn_item}>Terminal</p>
+          <p className={style.btn_item}>Help</p>
+        </div>
+      </div>
+      <p className={style.window_title}>Heeae Park - Visual Studio Code</p>
+      <div className={style.window_ic_wrap}>
+        <span className={`${style.window_ic} ${style.window_ic__red}`}></span>
+        <span className={`${style.window_ic} ${style.window_ic__yellow}`}></span>
+        <span className={`${style.window_ic} ${style.window_ic__green}`}></span>
+      </div>
     </div>
   )
 }
+
+export default WindowBar;
