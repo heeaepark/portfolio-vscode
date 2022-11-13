9638dcff80a58cefb08dd31eb97f9c535dbf93b3
heeaepark <pha060611@gmail.com>
2022-11-13 17:23:54 +0900

    axio 추가

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,12 @@
+import axios from 'axios'
+
 const Projects = () => {
+  axios.get('https://heeaepark.github.io/myapi/portfolio-api/project.json').then((결과)=>{
+    console.log(결과.data)
+  })
+  .catch(()=>{
+    console.log('실패함')
+  })
   return (
     <div>
       projects
